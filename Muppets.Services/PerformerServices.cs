90923769cd0808f5fe66f70ad578f170c2dc21a8
grimmer.jeremy@gmail.com <grimmer.jeremy@gmail.com>
2021-05-11 12:50:52 -0400

    basic services

--- Muppets.Services/PerformerServices.cs
+++ Muppets.Services/PerformerServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Muppets.Data;
+using Muppets.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,52 @@ namespace Muppets.Services
 {
     public class PerformerServices
     {
+        public bool CreatePerformer(PerformerCreate model)
+        {
+            var entity = new Performer()
+            {
+                PerformerName = model.PerformerName,
+                PerformerBirthdate = model.PerformerBirthdate
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Performers.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PerformerList> GetAllPerformers()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Performers.Select(e => new PerformerList()
+                {
+                    PerformerId = e.PerformerId,
+                    PerformerName = e.PerformerName
+                });
+                return query.ToArray();
+            }
+        }
+
+        public bool UpdatePerformer(PerformerUpdate model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Performers.Single(e => e.PerformerId == model.PerformerId);
+                entity.PerformerName = model.PerformerName;
+                entity.PerformerBirthdate = model.PerformerBirthdate;
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeletePerformer(int performerId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Performers.Single(e => e.PerformerId == performerId);
+                ctx.Performers.Remove(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
